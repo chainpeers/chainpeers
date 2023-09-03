72eb36566ab6ab1efede88584640f76439aa2a32
SoulFletcher <not1ikeother@mail.ru>
2023-09-03 12:17:49 +0300

    args to script need to be set as env vars

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,21 +1,29 @@
 import subprocess
-import argparse
 import threading
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--logloc", type=str)
-parser.add_argument("-j", "--jsonloc", type=str)
-args = parser.parse_args()
-log_loc = "--log.file=" + args.logloc
-json_loc = args.jsonloc
+import os
 
 
-class MyProcess:
+if os.environ.get('logloc') is not None:
+    log_loc = "--log.file=" + os.environ.get('logloc')
+else:
+    print('logloc in not set')
+if os.environ.get('jsonloc') is not None:
+    json_loc = os.environ.get('jsonloc')
+else:
+    print('jsonloc in not set')
+if os.environ.get('devloc') is not None:
+    dev_loc = '.' + os.environ.get('devloc')
+else:
+    print('devloc is not set')
+
+
+class CrawlerProcess:
     def __init__(self):
         self.proc = None
 
     def run_process(self):
-        self.proc = subprocess.Popen(["./go-ethereum/build/bin/devp2p", "--log.format=json", "--verbosity=5",log_loc, "discv4","crawl",json_loc])
+        self.proc = subprocess.Popen([dev_loc, "--log.format=json", "--verbosity=5",
+                                      log_loc, "discv4", "crawl", json_loc])
         self.proc.communicate()
 
     def get_input(self):
@@ -24,7 +32,7 @@ class MyProcess:
             self.proc.terminate()
 
 
-my_proc = MyProcess()
+my_proc = CrawlerProcess()
 
 
 process_thread = threading.Thread(target=my_proc.run_process)
